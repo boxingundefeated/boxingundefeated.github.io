6c9d17394513fba5eb4798bf3d59270bce3af92b
devinschumacher <devin@serp.co>
2025-08-09 07:05:08 -0700

    Remove auth middleware for static export
    - Static export doesn't support server actions
    - Boxing site doesn't need authentication
    - Middleware was importing auth package with 'use server'

--- apps/web/middleware.ts
+++
@@ -1,8 +0,0 @@
-import { middleware } from '@thedaviddias/auth'
-
-export const config = {
-  matcher:
-    '/((?!api|_next/static|_next/image|favicon.ico|robots.txt|llms.txt|opengraph-image.png|sitemap.xml|.*\\.).*)$'
-}
-
-export { middleware }
